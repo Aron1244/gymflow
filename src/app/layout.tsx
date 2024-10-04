097475ba61577b580ea1d6fc3c87fcd8c097f18e
Benjamin Herrera <raycuasa80@gmail.com>
2024-10-04 10:44:29 -0300

    Se movio la carpeta components y context

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-import { AuthProvider } from "./contexts/AuthContext";
+import Navbar from "../../components/Navbar";
+import Footer from "../../components/Footer";
+import { AuthProvider } from "../../contexts/AuthContext";
 
 const inter = Inter({ subsets: ["latin"] });
 
